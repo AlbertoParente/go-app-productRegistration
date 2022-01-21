aeec8a0125d479e5fa896d82bfed0c4ae51ef507
albertoparente <albertoparentefh@gmail.com>
2022-01-20 23:17:53 -0300

    Adding a Database Connection and Modifying template

--- main.go
+++ main.go
@@ -1,17 +1,43 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 	"text/template"
+
+	_ "github.com/lib/pq"
 )
+
+func ConnectDatabase() *sql.DB {
+	connection := "user=postgres dbname=product password=************ host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", connection)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
+type Product struct {
+	Name        string
+	Description string
+	Price       float64
+	Quantity    int
+}
 
 var templates = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
+	db := ConnectDatabase()
+	defer db.Close()
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "Index", nil)
+	products := []Product{
+		{"Apple", "Delicious", 1.00, 10},
+		{"Pineapple", "Very delicious", 3.00, 8},
+	}
+
+	templates.ExecuteTemplate(w, "Index", products)
 }
